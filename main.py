1ed6ec58c584096bc944ad0c0f96934009f81c24
Charles Zillmann <charles.zillmann@gmail.com>
2021-03-08 13:32:23 -0500

    test

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from DuplicateFileFinder import findDuplicates
 from FileOrganizer import organizeFiles
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    pathConsolidation = '/Volumes/D1-WDBLACK5TB/Consolidation'
+
+    findDuplicates(pathConsolidation)
     organizeFiles()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
