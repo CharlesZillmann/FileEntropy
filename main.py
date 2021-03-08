24dd9a917a9f2d6cda133ddc88868c9cd0e21a6f
Charles Zillmann <charles.zillmann@gmail.com>
2021-03-08 16:15:55 -0500

    test

--- main.py
+++ main.py
@@ -6,19 +6,38 @@
 ###########################################################################################################
 from DuplicateFileFinder import findDuplicates
 from FileOrganizer import organizeFiles
+import sys
 
 ###########################################################################################################
 # Name: main execution entry point
 # Parameters:
 ###########################################################################################################
 if __name__ == '__main__':
-    # The Path of the directory to be sorted
-    # path 			= '/Volumes/D1-WDBLACK5TB/Pictures'
-    pathConsol 	= '/Volumes/D1-WDBLACK5TB/Consolidation'
-    pathDest    = '/Volumes/D1-WDBLACK5TB/BitBucket'
+    if len(sys.argv) > 2:
+        print(f"Arguments count: {len(sys.argv)}")
+        for i, arg in enumerate(sys.argv):
+            print(f"Argument {i:>6}: {arg}")
 
-    findDuplicates( pathConsol )
-    organizeFiles( pathConsol, pathDest )
+        pathConsol = sys.argv[1]
+        pathDest = sys.argv[2]
+        print(f"Source Folder: {pathConsol}")
+        print(f"Destination Folder: {pathDest}")
+
+        # The Path of the directory to be sorted
+        # path 			= '/Volumes/D1-WDBLACK5TB/Pictures'
+        #pathConsol = '/Volumes/D1-WDBLACK5TB/Consolidation'
+        #pathDest = '/Volumes/D1-WDBLACK5TB/BitBucket'
+
+        findDuplicates(pathConsol)
+        organizeFiles(pathConsol, pathDest)
+    else:
+        print('Usage: python FileEntropy.py folder1 folder2')
+        print('Where: folder1 is the path to the source folder to be consolidated and folder2 is the path to the destination folder that will contain the consolidated files.')
+        print('Example: python FileEntropy.py /Volumes/D1-WDBLACK5TB/Consolidation /Volumes/D1-WDBLACK5TB/BitBucket')
+
+
+
+
 
 
 # if __name__ == '__main__':
